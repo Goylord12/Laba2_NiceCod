e7de867b353644898d41f212f0aaddb545ee3f90
Goylord12 <Renden15@yandex.ru>
2023-02-28 14:59:20 +0300

    аа

--- Laba_2ShitCode/obj/Debug/net6.0/Laba_2ShitCode.AssemblyInfo.cs
+++ Laba_2ShitCode/obj/Debug/net6.0/Laba_2ShitCode.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Laba_2FullShitCode")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Laba_2ShitCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Laba_2FullShitCode")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Laba_2FullShitCode")]
+[assembly: System.Reflection.AssemblyProductAttribute("Laba_2ShitCode")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Laba_2ShitCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
